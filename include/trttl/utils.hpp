e88c004160f521a1136f74b342c62b45ae25ce8c
OneAndZero24 <oneandzero24@gmail.com>
2025-01-08 19:21:15 +0100

    fix: very explicit dims for input

--- include/trttl/utils.hpp
+++ include/trttl/utils.hpp
@@ -26,17 +26,12 @@ private:
         builder = nvinfer1::createInferBuilder(logger);
         config = builder->createBuilderConfig();
         network = builder->createNetworkV2(1U << static_cast<uint32_t>(nvinfer1::NetworkDefinitionCreationFlag::kEXPLICIT_BATCH));
-        std::vector<int32_t> d(module.in_shape.d, module.in_shape.d+module.in_shape.nbDims);
-        d.insert(d.begin(), module.batch_size);
-        std::string msg = "Network input: ";
-        for (size_t i = 0; i < d.size(); ++i) {
-            msg += std::to_string(d[i]);
-            if (i < d.size() - 1) {
-                msg += ", ";
-            }
-        }
-        logger.log(trt_types::Severity::kINFO, msg);
-        auto input = network->addInput("input", trt_types::DataType::kFLOAT, trt_types::Dims{static_cast<int32_t>(d.size()), *d.data()});
+        trt_types::Dims in_dims;
+        in_dims.nbDims = module.in_shape.nbDims+1;
+        in_dims.d[0] = module.batch_size;
+        for (auto i = 0; i < module.in_shape.nbDims; ++i)
+            in_dims.d[i+1] = module.in_shape.d[i];
+        auto input = network->addInput("input", trt_types::DataType::kFLOAT, in_dims);
 
         trt_types::Tensor* output_tensor = module.addToNetwork(network, input);
         network->markOutput(*output_tensor);
